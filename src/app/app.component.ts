cab0f07762f2a815ed758d930965eda1275ee049
mkitez <user.pr@mail.ru>
2017-04-06 10:15:23 +0300

    Final working version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'my-app',
+  template: `
+    <h1>{{title}}</h1>
+    <router-outlet></router-outlet>`
 })
+
 export class AppComponent {
-  title = 'app works!';
+  title = 'Football Project';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,48 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import {Http, HttpModule, RequestOptions} from '@angular/http';
+
+import { TeamService } from './team.service';
+import { TeamListComponent } from './team-list.component';
+import { AppRoutingModule } from './app-routing.module';
+import { PlayerListComponent } from './player-list.component';
+import { PlayerComponent } from './player.component';
+import { LoginComponent } from './login.component';
+import { AuthGuard } from './auth-guard.service';
+import {AuthConfig, AuthHttp } from 'angular2-jwt';
+
+export function authHttpServiceFactory(http: Http, options: RequestOptions) {
+  return new AuthHttp(new AuthConfig({
+    tokenName: 'id_token',
+    tokenGetter: (() => localStorage.getItem('id_token')),
+    globalHeaders: [{ 'Content-Type' : 'application/json' }],
+  }), http, options);
+}
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
+      BrowserModule,
+      AppRoutingModule,
+      HttpModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+      AppComponent,
+      TeamListComponent,
+      PlayerListComponent,
+      PlayerComponent,
+      LoginComponent
+  ],
+  providers: [
+      TeamService,
+      AuthGuard,
+      {
+          provide: AuthHttp,
+          useFactory: authHttpServiceFactory,
+          deps: [Http, RequestOptions]
+      }
+  ],
+  bootstrap: [ AppComponent ]
 })
+
 export class AppModule { }
